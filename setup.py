61603061914d0ae92f6dd45c7ffd8d0f1a80ddd8
s-takahashijp <shallow.e1621@gmail.com>
2025-05-04 20:49:47 +0900

    05/04 setup.py fix

--- setup.py
+++ setup.py
@@ -5,10 +5,8 @@ package_name = 'waypoint_maker_pkg'
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[],
-    py_modules=[
-        'src.waypoint_maker',  # ← 'src.' を追加
-    ],
+    packages=[package_name], # パッケージ名を追加
+    py_modules=['src.waypoint_maker'],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Your Name',
@@ -16,9 +14,14 @@ setup(
     description='ROS 2 waypoint maker',
     license='Apache License 2.0',
     tests_require=['pytest'],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']), # package.xml を追加
+    ],
     entry_points={
         'console_scripts': [
-            'waypoint_maker = src.waypoint_maker:main',  # ← 'src.' を追加
+            'waypoint_maker = src.waypoint_maker:main',
         ],
     },
 )
